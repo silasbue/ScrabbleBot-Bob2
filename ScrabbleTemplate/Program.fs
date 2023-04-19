7598117f7540aa49ccef91070266fc697de9e488
Silas Bue Handgaard <silasbueh@hotmail.com>
2023-04-19 12:32:58 +0200

    Add bot name

--- ScrabbleTemplate/Program.fs
+++ ScrabbleTemplate/Program.fs
@@ -52,7 +52,7 @@ let main argv =
         time (fun () -> ScrabbleUtil.Dictionary.mkDict words dictAPI)
 
     // Uncomment this line to call your client
-    let players    = [("Your name here", dictionary, Bob2.Scrabble.startGame)]
+    let players    = [("ScrabBoble2", dictionary, Bob2.Scrabble.startGame)]
     
     // let players = spawnMultiples "OxyphenButazone" dictionary Oxyphenbutazone.Scrabble.startGame 2
 
